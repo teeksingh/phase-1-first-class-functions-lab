f31c68066ab75beef429bb4783e3f4bf4e12a9e4
Prateek <teeksingh@gmail.com>
2023-04-09 13:10:42 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2,4)
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    const multipliedFare = function(fare){
+        return multiplier * fare
+    } 
+    return multipliedFare
+}
+
+
+function fareDoubler(fare) {
+    const doubledFare = fare * 2
+    return doubledFare
+}
+
+function fareTripler(fare) {
+    const tripledFare = fare * 3
+    return tripledFare
+}
+
+function selectDifferentDrivers(drivers, functionName ) {
+    return functionName(drivers)
+    }
